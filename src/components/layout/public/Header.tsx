828d81f26030ecbaf6c7ded087bc4776eab3076a
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-09-16 23:26:33 +0600

    feat: implement user authentication hooks and enhance Header component with logout functionality and toast notifications

--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,5 +1,9 @@
+"use client"
 import Logo from "@/app/assets/svg/Logo";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/toast";
+import { useGetMe, useLogout } from "@/hooks";
+import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 
 export default function Header() {
@@ -8,10 +12,35 @@ export default function Header() {
         { name: "About us", url: "/about-us" },
     ];
 
+    const { data, isLoading } = useGetMe()
+    const { mutate: logout } = useLogout()
+
+    const queryClient = useQueryClient()
+
+    const handleLogout = () => {
+        logout(undefined, {
+            onSuccess: () => {
+                toast.add({
+                    title: "Logout successfully",
+                    description: "you have been logged out.",
+                    type: "success",
+                })
+                queryClient.removeQueries({ queryKey: ["user"] })
+            },
+            onError: (err) => {
+                toast.add({
+                    title: "Logout failed",
+                    description: "something went wrong. please try again.",
+                    type: "error"
+                })
+            }
+        })
+    }
+
     return (
         <header className="w-full h-16 border border-b">
             <div className="flex justify-between items-center h-full max-w-7xl mx-auto">
-                <Logo/>
+                <Logo />
                 <div>PH Healthcare</div>
                 <nav className="flex gap-5">
                     {routes.map((route) => (
@@ -21,13 +50,23 @@ export default function Header() {
                     ))}
                 </nav>
                 <div>
-                    <Button
-                        variant="outline"
-                        render={<Link href="/login">Login</Link>}
-                        nativeButton={false}
-                    >
-                        login
-                    </Button>
+                    {!data && !isLoading && (
+                        <Button
+                            variant="outline"
+                            render={<Link href="/login">Login</Link>}
+                            nativeButton={false}
+                        >
+                            login
+                        </Button>
+                    )}
+                    {!isLoading && data && (
+                        <Button
+                            variant="destructive"
+                            onClick={handleLogout}
+                        >
+                            logout
+                        </Button>
+                    )}
                 </div>
             </div>
         </header>
--- src/hooks/auth.hook.ts
+++ src/hooks/auth.hook.ts
@@ -1,8 +1,22 @@
-import { userLogin } from "@/api";
-import { useMutation } from "@tanstack/react-query";
+import { getMe, userLogin, userLogout } from "@/api";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useLogin() {
     return useMutation({
         mutationFn: userLogin,
+    })
+}
+
+export function useLogout() {
+    return useMutation({
+        mutationFn: userLogout,
+    })
+}
+
+export function useGetMe() {
+    return useQuery({
+        queryKey: ["user"],
+        queryFn: getMe,
+        retry: false
     })
 }
