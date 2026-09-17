17295b7a7f19792482e4c0984c5be33180bdd650
tashrif-miyaji <tashrifmiyaji@gmail.com>
2026-09-18 00:55:50 +0600

    fix: correct parameter type for userLogin function and adjust formatting for consistency

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,16 +1,16 @@
 import apiClient from "@/lib/apiClient";
 import { LoginPayload, RegistrationPayload, VerifyAccountPayload } from "@/types";
 
-export function userLogin(payload: { email: LoginPayload }) {
+export function userLogin(payload: LoginPayload) {
     return apiClient("/auth/login", { method: "POST", body: payload })
 }
 
 export function userRegistration(payload: RegistrationPayload) {
-  return apiClient("/auth/register", { method: "POST", body: payload });
+    return apiClient("/auth/register", { method: "POST", body: payload });
 }
 
 export function verifyAccount(payload: VerifyAccountPayload) {
-  return apiClient("/auth/verify-email", { method: "POST", body: payload });
+    return apiClient("/auth/verify-email", { method: "POST", body: payload });
 }
 
 export function userLogout() {
